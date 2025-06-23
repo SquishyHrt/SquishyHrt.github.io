6fb36bcfc7e6dd7c329cfcdcfb889b171b8df8f8
Paolo Wattebled <paolo.wattebled@epita.fr>
2025-06-23 21:38:50 +0200

    build: init commit with portfolio template

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+    site: 'https://SquishyHrt.github.io',
+    // Don't put base because this is a root project
+});
